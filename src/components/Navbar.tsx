df7875e1eef2af2ed115341caf9e0f70cd20a374
Muhammed Soliman <152155286+Mhhamd@users.noreply.github.com>
2025-08-17 11:54:27 +0300

    Create Navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,26 @@
+import Link from 'next/link';
+import DesktopNavbar from './DesktopNavbar';
+import MobileNavbar from './MobileNavbar';
+
+function Navbar() {
+  return (
+    <header className="sticky top-0 border-b w-full py-5 bg-background/95 backdrop-blur-2xl supports-[backdrop-filter]:bg-background/60 z-50">
+      <div className=" mx-auto max-w-7xl flex items-center justify-between px-4">
+        {/* LEFT SECTION */}
+        <div className="flex items-center w-full">
+          <Link
+            href={'/'}
+            className=" text-lg sm:text-xl font-bold text-primary font-mono tracking-wider cursor-pointer hover:opacity-70 duration-300">
+            <h1>Socially</h1>
+          </Link>
+        </div>
+
+        {/* RIGHT SECTION */}
+        <DesktopNavbar />
+        <MobileNavbar />
+      </div>
+    </header>
+  );
+}
+
+export default Navbar;
