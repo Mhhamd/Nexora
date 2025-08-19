4bb66e33bfe8cda9a90aee0b1b071604031dfdc7
Muhammed Soliman <152155286+Mhhamd@users.noreply.github.com>
2025-08-19 11:31:06 +0300

    Better auth setup

---
+++ src/app/sign-in/page.tsx
@@ -0,0 +1,9 @@
+function SignInPage() {
+  return (
+    <div className="bg/background flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+      <div className="flex w-full max-w-sm flex-col gap-6">LoginComp</div>
+    </div>
+  );
+}
+
+export default SignInPage;
