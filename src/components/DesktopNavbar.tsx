7c19cbe6678b1eb227a3ee2b92e3bc0907cd7c16
Muhammed Soliman <152155286+Mhhamd@users.noreply.github.com>
2025-08-20 14:01:50 +0300

    Add session check

--- src/components/DesktopNavbar.tsx
+++ src/components/DesktopNavbar.tsx
@@ -2,8 +2,10 @@ import { BellIcon, HomeIcon, User } from 'lucide-react';
 import ModeToggle from './ModeToggle';
 import { Button } from './ui/button';
 import Link from 'next/link';
+import { getSession } from '@/server/session';
 
-function DesktopNavbar() {
+async function DesktopNavbar() {
+  const session = await getSession();
   return (
     <div className="sm:flex items-center gap-5 hidden">
       <ModeToggle />
@@ -15,21 +17,37 @@ function DesktopNavbar() {
         </Link>
       </Button>
 
-      {/* TODO: Render only if there's a user */}
-      <Button variant={'ghost'} className="cursor-pointer" asChild>
-        <Link href={'/notifications'}>
-          <BellIcon />
-          <span className="lg:inline hidden">Notifications</span>
-        </Link>
-      </Button>
+      {session ? (
+        <>
+          <Button variant={'ghost'} className="cursor-pointer" asChild>
+            <Link href={'/notifications'}>
+              <BellIcon />
+              <span className="lg:inline hidden">Notifications</span>
+            </Link>
+          </Button>
 
-      {/* PROFILE */}
-      <Button variant={'ghost'} className="cursor-pointer" asChild>
-        <Link href={`/profile`}>
-          <User />
-          <span className="lg:inline hidden">Profile</span>
-        </Link>
-      </Button>
+          <Button variant={'ghost'} className="cursor-pointer" asChild>
+            <Link href={`/profile`}>
+              <User />
+              <span className="lg:inline hidden">Profile</span>
+            </Link>
+          </Button>
+          {/* TODO: Add user menu */}
+        </>
+      ) : (
+        <>
+          <Button variant={'default'} className="cursor-pointer" asChild>
+            <Link href={`/login`}>
+              <span className="lg:inline hidden">Login</span>
+            </Link>
+          </Button>
+          <Button variant={'secondary'} className="cursor-pointer" asChild>
+            <Link href={`/sign-up`}>
+              <span className="lg:inline hidden">Sign up</span>
+            </Link>
+          </Button>
+        </>
+      )}
     </div>
   );
 }
