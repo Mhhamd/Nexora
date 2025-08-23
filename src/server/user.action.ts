4ef2a2fcebb7a4de3ea69b5b1027493c3db4ef92
Muhammed Soliman <152155286+Mhhamd@users.noreply.github.com>
2025-08-23 12:12:32 +0300

    Create user action

---
+++ src/server/user.action.ts
@@ -0,0 +1,23 @@
+'use server';
+import { prisma } from '@/lib/prisma';
+import { getSession } from './session';
+
+export async function getCurrentUser() {
+  const session = await getSession();
+  if (!session) return;
+
+  const user = await prisma.user.findUnique({
+    where: { id: session.user.id },
+    select: {
+      id: true,
+      name: true,
+      email: true,
+      image: true,
+      website: true,
+      location: true,
+      bio: true,
+      createdAt: true,
+    },
+  });
+  return user;
+}
