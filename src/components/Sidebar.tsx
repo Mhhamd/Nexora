aa89681804c759e5e2f0202595a97b6aa0acff7c
Muhammed Soliman <152155286+Mhhamd@users.noreply.github.com>
2025-08-24 11:33:03 +0300

    Create Siebar

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,99 @@
+'use client';
+
+import { Card, CardContent } from '@/components/ui/card';
+import Link from 'next/link';
+import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
+import { getCurrentUser } from '@/server/user.action';
+import { useEffect, useState } from 'react';
+import SidebarSkeleton from './skeletons/SidebarSkeleton';
+import { Separator } from './ui/separator';
+import { LinkIcon, MapPinIcon } from 'lucide-react';
+
+type User = Awaited<ReturnType<typeof getCurrentUser>>;
+
+function Sidebar() {
+  const [user, setUser] = useState<User | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      setIsLoading(true);
+      try {
+        const res = await fetch('/api/user');
+        const data = await res.json();
+        setUser(data);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchUser();
+  }, []);
+  return (
+    <div className="sticky top-20">
+      <Card className="bg-background ">
+        <CardContent>
+          <div className="flex items-center justify-center flex-col">
+            {isLoading ? (
+              <SidebarSkeleton />
+            ) : (
+              <Link
+                className="flex flex-col items-center justify-center w-full"
+                href={`/profile/${user?.email.split('@')[0]}`}
+                prefetch>
+                <Avatar className="size-15 border-2">
+                  <AvatarImage src={user?.image ?? undefined} alt={user?.name || ''} />
+                  <AvatarFallback>{user?.name?.[0] || user?.email?.split('@')[0]}</AvatarFallback>
+                </Avatar>
+
+                <div className="mt-4 space-y-1 text-center">
+                  <h3 className="font-semibold">{user?.name}</h3>
+                  <p className="text-center text-muted-foreground">{user?.email.split('@')[0]}</p>
+                </div>
+
+                {user?.bio && <p className="mt-3 text-sm text-muted-foreground">{user.bio}</p>}
+              </Link>
+            )}
+            <div className="w-full">
+              <Separator className="my-4" />
+
+              <div className="flex justify-between text-center">
+                <div>
+                  <p className="font-medium">{user?._count.following}</p>
+                  <p className="text-xs text-muted-foreground">Following</p>
+                </div>
+                <Separator orientation="vertical" />
+                <div>
+                  <p className="font-medium">{user?._count.followers}</p>
+                  <p className="text-xs text-muted-foreground">Followers</p>
+                </div>
+              </div>
+              <Separator className="my-4" />
+
+              <div className="w-full space-y-2 text-sm">
+                <div className="flex items-center text-muted-foreground">
+                  <MapPinIcon className="size-4 mr-2" />
+                  {user?.location || 'No Location'}
+                </div>
+                <div className="flex items-center text-muted-foreground">
+                  <LinkIcon className="size-4 mr-2" />
+                  {user?.website ? (
+                    <a href={user?.website} className="hover:underline truncate" target="_blank">
+                      {user.website}
+                    </a>
+                  ) : (
+                    'No Website'
+                  )}
+                </div>
+              </div>
+            </div>
+          </div>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
+
+export default Sidebar;
