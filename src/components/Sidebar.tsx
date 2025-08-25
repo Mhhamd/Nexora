a9e21246a38347c35b3c016a790ba962be87061c
Muhammed Soliman <152155286+Mhhamd@users.noreply.github.com>
2025-08-25 11:55:29 +0300

    Refactor Sidebar to use Redux

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,73 +3,49 @@
 import { Card, CardContent } from '@/components/ui/card';
 import Link from 'next/link';
 import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
-import { getCurrentUser } from '@/server/user.action';
-import { useEffect, useState } from 'react';
-import SidebarSkeleton from './skeletons/SidebarSkeleton';
 import { Separator } from './ui/separator';
 import { LinkIcon, MapPinIcon } from 'lucide-react';
 import { Button } from './ui/button';
-
-type User = Awaited<ReturnType<typeof getCurrentUser>>;
+import { useSelector } from 'react-redux';
+import { RootState } from '@/redux/store';
 
 function Sidebar() {
-  const [user, setUser] = useState<User | null>(null);
-  const [isLoading, setIsLoading] = useState(false);
+  const { isAuthenticated, user } = useSelector((state: RootState) => state.user);
 
-  //   TODO: Fix unnecessary fetching
-  useEffect(() => {
-    const fetchUser = async () => {
-      setIsLoading(true);
-      try {
-        const res = await fetch('/api/user');
-        const data = await res.json();
-        setUser(data);
-      } catch (error) {
-        console.error(error);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-
-    fetchUser();
-  }, []);
   return (
     <div className="sticky top-20">
       <Card className="bg-background ">
         <CardContent>
-          {user ? (
+          {isAuthenticated ? (
             <div className="flex items-center justify-center flex-col">
-              {isLoading ? (
-                <SidebarSkeleton />
-              ) : (
-                <Link
-                  className="flex flex-col items-center justify-center w-full"
-                  href={`/profile/${user?.email.split('@')[0]}`}
-                  prefetch>
-                  <Avatar className="size-15 border-2">
-                    <AvatarImage src={user?.image ?? undefined} alt={user?.name || ''} />
-                    <AvatarFallback>{user?.name?.[0] || user?.email?.split('@')[0]}</AvatarFallback>
-                  </Avatar>
+              <Link
+                className="flex flex-col items-center justify-center w-full"
+                href={`/profile/${user?.email.split('@')[0]}`}
+                prefetch>
+                <Avatar className="size-15 border-2">
+                  <AvatarImage src={user?.image ?? undefined} alt={user?.name || ''} />
+                  <AvatarFallback>{user?.name?.[0] || user?.email?.split('@')[0]}</AvatarFallback>
+                </Avatar>
 
-                  <div className="mt-4 space-y-1 text-center">
-                    <h3 className="font-semibold">{user?.name}</h3>
-                    <p className="text-center text-muted-foreground">{user?.email.split('@')[0]}</p>
-                  </div>
+                <div className="mt-4 space-y-1 text-center">
+                  <h3 className="font-semibold">{user?.name}</h3>
+                  <p className="text-center text-muted-foreground">{user?.email.split('@')[0]}</p>
+                </div>
 
-                  {user?.bio && <p className="mt-3 text-sm text-muted-foreground">{user.bio}</p>}
-                </Link>
-              )}
+                {user?.bio && <p className="mt-3 text-sm text-muted-foreground">{user.bio}</p>}
+              </Link>
+
               <div className="w-full">
                 <Separator className="my-4" />
 
                 <div className="flex justify-between text-center">
                   <div>
-                    <p className="font-medium">{user?._count.following}</p>
+                    <p className="font-medium">{user?.following}</p>
                     <p className="text-xs text-muted-foreground">Following</p>
                   </div>
                   <Separator orientation="vertical" />
                   <div>
-                    <p className="font-medium">{user?._count.followers}</p>
+                    <p className="font-medium">{user?.followers}</p>
                     <p className="text-xs text-muted-foreground">Followers</p>
                   </div>
                 </div>
