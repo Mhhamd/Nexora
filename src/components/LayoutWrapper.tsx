fde5cae15f5d887d4108403b1fd5c86fc0c0d19f
Muhammed Soliman <152155286+Mhhamd@users.noreply.github.com>
2025-08-26 10:58:24 +0300

    Optimize user fetching in LayoutWrapper

--- src/components/LayoutWrapper.tsx
+++ src/components/LayoutWrapper.tsx
@@ -2,10 +2,31 @@
 
 import { usePathname } from 'next/navigation';
 import Sidebar from '@/components/Sidebar';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { authClient } from '@/lib/auth-client';
+import { getCurrentUser } from '@/server/user.action';
+import { setUser } from '@/redux/slices/userSlice';
+import { RootState } from '@/redux/store';
 
 export default function LayoutWrapper({ children }: { children: React.ReactNode }) {
   const pathname = usePathname();
+  const dispatch = useDispatch();
+  const user = useSelector((state: RootState) => state.user.user);
   const hideLayout = pathname === '/login' || pathname === '/sign-up';
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      if (!user) {
+        const fetchedUser = await getCurrentUser();
+        if (fetchedUser) {
+          dispatch(setUser(fetchedUser));
+        }
+      }
+    };
+
+    fetchUser();
+  }, [dispatch, user]);
 
   if (hideLayout) {
     return <>{children}</>;
