f231b7557b83b0b1894d06d71850a2a3f17a6b90
Muhammed Soliman <152155286+Mhhamd@users.noreply.github.com>
2025-08-29 11:55:30 +0300

    Update sidebar style

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,35 +1,35 @@
-'use client';
+"use client";
 
-import { Card, CardContent } from '@/components/ui/card';
-import Link from 'next/link';
-import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
-import { Separator } from './ui/separator';
-import { LinkIcon, MapPinIcon } from 'lucide-react';
-import { Button } from './ui/button';
-import { useSelector } from 'react-redux';
-import { RootState } from '@/redux/store';
+import { Card, CardContent } from "@/components/ui/card";
+import Link from "next/link";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { Separator } from "./ui/separator";
+import { LinkIcon, MapPinIcon } from "lucide-react";
+import { Button } from "./ui/button";
+import { useSelector } from "react-redux";
+import { RootState } from "@/redux/store";
 
 function Sidebar() {
   const { isAuthenticated, user } = useSelector((state: RootState) => state.user);
 
   return (
-    <div className="sticky top-20">
+    <div className="sticky top-25">
       <Card className="bg-background ">
         <CardContent>
           {isAuthenticated ? (
             <div className="flex items-center justify-center flex-col">
               <Link
                 className="flex flex-col items-center justify-center w-full"
-                href={`/profile/${user?.email.split('@')[0]}`}
+                href={`/profile/${user?.email.split("@")[0]}`}
                 prefetch>
                 <Avatar className="size-15 border-2">
-                  <AvatarImage src={user?.image ?? undefined} alt={user?.name || ''} />
-                  <AvatarFallback>{user?.name?.[0] || user?.email?.split('@')[0]}</AvatarFallback>
+                  <AvatarImage src={user?.image ?? undefined} alt={user?.name || ""} />
+                  <AvatarFallback>{user?.name?.[0] || user?.email?.split("@")[0]}</AvatarFallback>
                 </Avatar>
 
                 <div className="mt-4 space-y-1 text-center">
                   <h3 className="font-semibold">{user?.name}</h3>
-                  <p className="text-center text-muted-foreground">{user?.email.split('@')[0]}</p>
+                  <p className="text-center text-muted-foreground">{user?.email.split("@")[0]}</p>
                 </div>
 
                 {user?.bio && <p className="mt-3 text-sm text-muted-foreground">{user.bio}</p>}
@@ -54,7 +54,7 @@ function Sidebar() {
                 <div className="w-full space-y-2 text-sm">
                   <div className="flex items-center text-muted-foreground">
                     <MapPinIcon className="size-4 mr-2" />
-                    {user?.location || 'No Location'}
+                    {user?.location || "No Location"}
                   </div>
                   <div className="flex items-center text-muted-foreground">
                     <LinkIcon className="size-4 mr-2" />
@@ -63,7 +63,7 @@ function Sidebar() {
                         {user.website}
                       </a>
                     ) : (
-                      'No Website'
+                      "No Website"
                     )}
                   </div>
                 </div>
@@ -79,13 +79,13 @@ function Sidebar() {
                   Login to access your profile and connect with others.
                 </p>
                 <div className="space-y-3">
-                  <Button variant={'ghost'} className="cursor-pointer w-full border" asChild>
-                    <Link href={'/login'} prefetch>
+                  <Button variant={"ghost"} className="cursor-pointer w-full border" asChild>
+                    <Link href={"/login"} prefetch>
                       Login
                     </Link>
                   </Button>
-                  <Button variant={'default'} className="cursor-pointer w-full " asChild>
-                    <Link href={'sign-up'} prefetch>
+                  <Button variant={"default"} className="cursor-pointer w-full " asChild>
+                    <Link href={"sign-up"} prefetch>
                       Sign up
                     </Link>
                   </Button>
