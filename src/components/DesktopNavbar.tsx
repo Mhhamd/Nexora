0709bce41195b4277786923de8c73aa59789c609
Muhammed Soliman <152155286+Mhhamd@users.noreply.github.com>
2025-09-07 15:27:05 +0300

    Add unread notifications badge to DesktopNavbar
    Introduces a badge on the notifications button to display the count of unread notifications for authenticated users. Fetches notifications on mount and when navigating to the notifications page, and handles errors with a toast message.

--- src/components/DesktopNavbar.tsx
+++ src/components/DesktopNavbar.tsx
@@ -1,21 +1,48 @@
-'use client';
-import { BellIcon, HomeIcon, LogInIcon, UserPlusIcon } from 'lucide-react';
-import ModeToggle from './ModeToggle';
-import { Button } from './ui/button';
-import Link from 'next/link';
-import UserMenu from './UserMenu';
-import { useSelector } from 'react-redux';
-import { RootState } from '@/redux/store';
+"use client";
+import { BellIcon, HomeIcon, LogInIcon, UserPlusIcon } from "lucide-react";
+import ModeToggle from "./ModeToggle";
+import { Button } from "./ui/button";
+import Link from "next/link";
+import UserMenu from "./UserMenu";
+import { useSelector } from "react-redux";
+import { RootState } from "@/redux/store";
+import { useEffect, useState } from "react";
+import { toast } from "sonner";
+import { getNotifications } from "@/server/notifications.action";
+import { usePathname } from "next/navigation";
 
 function DesktopNavbar() {
   const { isAuthenticated, user } = useSelector((state: RootState) => state.user);
+  const [unreadNotifications, setUnreadNotifications] = useState(0);
+  const pathName = usePathname();
 
+  useEffect(() => {
+    if (!user || !isAuthenticated) return;
+    const fetchNotifications = async () => {
+      try {
+        const data = await getNotifications();
+        if (data) {
+          const notificationsNumber = data.filter((n) => !n.read);
+          setUnreadNotifications(notificationsNumber.length);
+        }
+      } catch (error) {
+        toast.error("Failed to fetch notifications");
+      }
+    };
+    fetchNotifications();
+
+    if (pathName === "/notifications") {
+      const handleRouteChange = () => fetchNotifications();
+      window.addEventListener("popstate", handleRouteChange);
+      return () => window.removeEventListener("popstate", handleRouteChange);
+    }
+  }, [isAuthenticated, user, pathName]);
   return (
     <div className="sm:flex items-center gap-5 hidden">
       <ModeToggle />
 
-      <Button variant={'ghost'} className="cursor-pointer" asChild>
-        <Link href={'/'}>
+      <Button variant={"ghost"} className="cursor-pointer" asChild>
+        <Link href={"/"}>
           <HomeIcon />
           <span className="lg:inline hidden">Home</span>
         </Link>
@@ -23,10 +50,15 @@ function DesktopNavbar() {
 
       {isAuthenticated && user ? (
         <>
-          <Button variant={'ghost'} className="cursor-pointer" asChild>
-            <Link href={'/notifications'}>
+          <Button variant={"ghost"} className="cursor-pointer relative" asChild>
+            <Link href={"/notifications"}>
               <BellIcon />
               <span className="lg:inline hidden">Notifications</span>
+              {unreadNotifications !== 0 && (
+                <span className="absolute -right-1 -top-1 rounded-full bg-accent-foreground size-4 text-xs text-black text-center leading-tight">
+                  {unreadNotifications}
+                </span>
+              )}
             </Link>
           </Button>
 
@@ -34,13 +66,13 @@ function DesktopNavbar() {
         </>
       ) : (
         <>
-          <Button variant={'default'} className="cursor-pointer" asChild>
+          <Button variant={"default"} className="cursor-pointer" asChild>
             <Link href={`/login`} prefetch>
               <LogInIcon />
               <span className="lg:inline hidden">Login</span>
             </Link>
           </Button>
-          <Button variant={'secondary'} className="cursor-pointer" asChild>
+          <Button variant={"secondary"} className="cursor-pointer" asChild>
             <Link href={`/sign-up`} prefetch>
               <UserPlusIcon />
               <span className="lg:inline hidden">Sign up</span>
