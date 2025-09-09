f41338baf987823eb048a207ef28e61b90c9be16
Muhammed Soliman <152155286+Mhhamd@users.noreply.github.com>
2025-09-09 11:38:19 +0300

    Update gradient color

--- src/components/NoChatSelected.tsx
+++ src/components/NoChatSelected.tsx
@@ -6,7 +6,7 @@ function NoChatSelected() {
     <div className="w-full h-full flex items-center justify-center flex-col animate-pulse ">
       <div className="flex items-center gap-2 p-4 bg-background border-b ">
         <MessageCircle className="h-8 w-8 text-primary" />
-        <h1 className="text-2xl font-bold bg-gradient-to-r from-white to-gray-800  bg-clip-text text-transparent">
+        <h1 className="text-2xl font-bold bg-gradient-to-r from-primary to-gray-800  bg-clip-text text-transparent">
           Nexora
         </h1>
       </div>
