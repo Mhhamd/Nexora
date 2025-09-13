d44b8f8e43159b14d2c90f3b48d03e9fcc1d737b
Muhammed Soliman <152155286+Mhhamd@users.noreply.github.com>
2025-09-13 10:55:08 +0300

    fix: add baseURL

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,12 +1,12 @@
-import { betterAuth } from 'better-auth';
-import { prismaAdapter } from 'better-auth/adapters/prisma';
-import { PrismaClient } from '@prisma/client';
-import { nextCookies } from 'better-auth/next-js';
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { PrismaClient } from "@prisma/client";
+import { nextCookies } from "better-auth/next-js";
 
 const prisma = new PrismaClient();
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
-    provider: 'postgresql',
+    provider: "postgresql",
   }),
   emailAndPassword: {
     enabled: true,
@@ -17,6 +17,6 @@ export const auth = betterAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     },
   },
-
+  baseURL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL || process.env.BETTER_AUTH_URL,
   plugins: [nextCookies()],
 });
